dd950873aa0ab7513d048ab2d90a2b2bad2a72b2
Pablo <pablo@intvs.org>
2019-10-16 10:34:16 +0100

    Init App

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,10 +1,1 @@
-// Logic for personal Site//
-
-/* Layout 
-[x] Toolbar -> Logo, DrawerToggle, Nav
-[x] SideDrawer -> Nav
-[x] DrawerToggle -> Open SideDrawer
-[x] Backdrop -> Close SideDrawer
-*/
-
-// Set up eventEmitters
+import '../styles.css';
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,50 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.export = {
+module.exports = {
   entry: path.resolve(__dirname, 'src', 'public', 'js', 'index.js'),
+  plugins: [
+    new HtmlWebpackPlugin({
+    template: './src/public/index.html',
+          }),
+  ],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
-  mode:'development',
+  mode:'production',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    hot: true,
+    port: 9000
+  },
   module: {
     rules: [
       {
-        test: /\.js*/,
+        test: /\.js$/,
         exclude: /node_modules/,
         loader: [
           {
           loader: 'babel-loader'
         }
       ]
-      }
-    ]
-  }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: [
+          {
+          loader: 'eslint-loader'
+        }
+      ]
+      },
+      {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader'],
+      exclude: /\.module\.css$/
+    },
+    ],
+  },
 }
 
