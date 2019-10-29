b4affc23162289ac46e964b2a0fc9c2ae4bcb45a
Pablo <pablo@intvs.org>
2019-10-29 16:52:15 +0000

    remove files

--- src/public/js/getRoles/getRoles.js
+++
@@ -1,9 +0,0 @@
-const getRoles = (n) => {
-  const roles = document.getElementsByClassName('Role');
-  for (let i = 0; i < roles.length; i += 1) {
-    roles[i].style.display = 'none';
-  }
-  roles[n].style.display = 'block';
-};
-
-export default getRoles;
